139268b8794ccafec98f61eb36892a958b8c8933
Roman Mendaliev <reacheight8@gmail.com>
2025-08-25 22:50:01 +0300

    dont show modals for dev

--- frontend/src/components/Level/Level.js
+++ frontend/src/components/Level/Level.js
@@ -97,9 +97,9 @@ export const Level = () => {
 
   const { user, isLoading: isUserLoading, isAuthenticated } = useUser();
 
-  const showLoginModal = !isUserLoading && !isAuthenticated;
+  const showLoginModal = process.env.REACT_APP_SHOW_LOGIN_MODAL && !isUserLoading && !isAuthenticated;
   const showAccessModal = false && id > 6 && !!user && !user.hasAccess;
-  const showPreviousLevelsModal = !showAccessModal && !isUserLoading && !isGameDataLoading && isAuthenticated && completedLevelsCount + 1 < id;
+  const showPreviousLevelsModal = process.env.REACT_APP_SHOW_PREV_LEVEL_MODAL && !showAccessModal && !isUserLoading && !isGameDataLoading && isAuthenticated && completedLevelsCount + 1 < id;
 
   const hasGuid = (data) =>
     data.instructions || data.example || data.newCommands?.length;
